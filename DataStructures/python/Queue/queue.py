961038367c533c131e89f5ee09df9e5aa5504c67
Mikhail Hyde <hyde.mikhail@gmail.com>
2020-01-25 16:57:58 -0500

    Added Queue

--- DataStructures/python/Queue/queue.py
+++ DataStructures/python/Queue/queue.py
@@ -3,5 +3,57 @@
 from node import Node
 
 class Queue:
-    def __init__(self):
-        pass
+    def __init__(self, max_size=None):
+        self.head = None
+        self.tail = None
+        self.max_size = max_size
+        self.size = 0
+
+    def enqueue(self, data):
+        if self.has_space():
+            new_node = Node(data)
+            if self.is_empty():
+                self.head = new_node
+                self.tail = new_node
+            else:
+                self.tail.set_next_node(new_node)
+                self.tail = new_node
+            self.size += 1
+        else:
+            print('Queue Overflow Protected. Node not Enqueued.')
+
+    def dequeue(self):
+        if not self.is_empty():
+            item_to_remove = self.head
+            if self.size == 1:
+                self.head = None
+                self.tail = None
+            else:
+                self.head = self.head.get_next_node()
+            self.size -= 1
+            return item_to_remove.get_data()
+        else:
+            print('Queue Underflow Protected. Nothing to Dequeue.')
+
+    def has_space(self):
+        return self.size < self.max_size
+
+    def is_empty(self):
+        return self.size == 0
+
+if __name__ == '__main__':
+    queue = Queue(5)
+    queue.enqueue('A')
+    queue.enqueue('B')
+    queue.enqueue('C')
+    queue.enqueue('D')
+    queue.enqueue('E')
+    print('\n')
+    queue.enqueue('F')
+
+    print(queue.dequeue())
+    print(queue.dequeue())
+    print(queue.dequeue())
+    print(queue.dequeue())
+    print(queue.dequeue())
+    print(queue.dequeue())
